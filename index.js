267206709e18c1d366f3ba49c83728650d293b64
abokhedr081-debug <abokhedr081@gmail.com>
2026-07-25 23:56:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const mineflayer = require('mineflayer');
+
 const bot = mineflayer.createBot({
   host: 'TK_COMMUNITY.aternos.me',
   port: 64764,
   username: 'AfnBot'
 });
+
 bot.on('spawn', () => {
-  console.log("البوت دخل السيرفر!");
+  console.log("البوت دخل السيرفر بنجاح!");
+  
+  // حركة بسيطة كل 10 ثواني عشان ما يفصلش بسبب الخمول
   setInterval(() => {
     bot.setControlState('jump', true);
     setTimeout(() => bot.setControlState('jump', false), 500);
   }, 10000);
 });
+
+bot.on('error', (err) => {
+  console.log("حدث خطأ:", err);
+});
+
 bot.on('end', () => {
-  setTimeout(() => process.exit(1), 5000);
+  console.log("البوت خرج، جاري إعادة المحاولة...");
+  setTimeout(() => {
+    process.exit(1);
+  }, 5000);
 });
